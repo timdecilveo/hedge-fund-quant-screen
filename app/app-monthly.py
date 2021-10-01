ee2838719fef13b4bf6ba4d0d5bf1dbcf145c6c1
timdecilveo <timothy.decilveo@gmail.com>
2021-10-01 12:58:45 -0700

    MAIN-added risk statistics

--- app/app-monthly.py
+++ app/app-monthly.py
@@ -11,6 +11,7 @@ from sklearn import metrics
 # set the 'Date' column as the index
 monthly_data = pd.read_csv("../data/hf_monthly.csv", parse_dates=["Date"], index_col="Date")
 df_monthly = pd.DataFrame(monthly_data)
+# df_ind= pd.DataFrame(pd.read_csv("../data/hf_monthly.csv", parse_dates=["Date"]))
 
 def plots(data_frame):
     plt.figure()
@@ -22,61 +23,69 @@ def plots(data_frame):
 cols = df_monthly.columns
 
 def performance_statistics(data_frame):
-    # #
-    # Performance Statistics
-    # #
+    # ### Statistics
     average_return = data_frame[cols].mean()
     median_return = data_frame[cols].median()
     max_return = data_frame[cols].max()
     min_return = data_frame[cols].min()
     st_dev = data_frame[cols].std()
 
-    print(f"average_return:\n{average_return}")
-    print(f"----")
-    print(f"median_return:\n{median_return}")
-    print(f"----")
-    print(f"max_return:\n{max_return}")
-    print(f"----")
-    print(f"min_return:\n{min_return}")
-    print(f"----")
-    print(f"st_dev:\n{st_dev}")
-    print(f"---------------------")
-
-    # #
-    # Risk Ratios
-    # #
+    # ### Risk Ratios
     average_return_over_st_dev = average_return / st_dev
-    # gain_to_pain = df_monthly[cols].mean()
+    gain_to_pain = df_monthly[cols].sum() / abs(df_monthly[df_monthly[cols] < 0].sum())
     # information_ratio = df_monthly[cols].mean()
     skew = df_monthly[cols].skew(axis=0)
-    kurtosis = df_monthly[cols].kurtosis(axis=0) # https://www.usepandas.com/data-analysis/kurtosis
+    kurtosis = df_monthly[cols].kurtosis(axis=0)
     excess_kurtosis = kurtosis - 3
+    kurtosis_times_skew = kurtosis * skew
+    excess_kurtosis_times_skew = excess_kurtosis * skew
+    # downside_deviation = 
+    # tail_ratio = 
+    # treynor_ratio = 
 
-    # if kurtosis < 0 & skew < 0:
-    #     kurtosis_times_skew = -(kurtosis * skew)
-    # else:
-    #     kurtosis_times_skew =kurtosis * skew
-    # kurtosis_times_skew = -(kurtosis * skew) if kurtosis < 0 and skew < 0 else (kurtosis * skew)
-    # excess_kurtosis_times_skew = -(excess_kurtosis*skew) if excess_kurtosis < 0 and skew < 0 else (excess_kurtosis*skew)
 
-    print(f"average_return_over_st_dev:\n{average_return_over_st_dev}")
-    print(f"----")
-    # print(f"gain_to_pain:\n{gain_to_pain}")
-    # print(f"----")
-    # print(f"information_ratio:\n{information_ratio}")
-    # print(f"----")
-    print(f"skew:\n{skew}")
-    print(f"----")
-    print(f"kurtosis:\n{kurtosis}")
-    print(f"----")
-    print(f"excess_kurtosis:\n{excess_kurtosis}")
-    print(f"----")
-    # print(f"kurtosis_times_skew:\n{kurtosis_times_skew}")
-    print(f"----")
-    # print(f"excess_kurtosis_times_skew:\n{excess_kurtosis_times_skew}")
-    print(f"----")
+    # # print(f"information_ratio:\n{information_ratio}")
+    # # print(f"----")
 
-    print(f"---------------------")
+    # ### Convert to DataFrames
+    df_average_return = pd.DataFrame([average_return], index=['Average Return'])
+    df_median_return = pd.DataFrame([median_return], index=['Median Return'])
+    df_max_return = pd.DataFrame([max_return], index=['Maximum Return'])
+    df_min_return = pd.DataFrame([min_return], index=['Minimum Return'])
+    df_st_dev = pd.DataFrame([st_dev], index=['Standard Deviation'])
+    df_average_return_over_st_dev = pd.DataFrame([average_return_over_st_dev], index=['Avg. Return / Std. Dev.'])
+    df_gain_to_pain = pd.DataFrame([gain_to_pain], index=['Gain to Pain Ratio'])
+    # df_information_ratio
+    df_skew = pd.DataFrame([skew], index=['Skew'])
+    df_kurtosis = pd.DataFrame([kurtosis], index=['Kurtosis'])
+    df_excess_kurtosis = pd.DataFrame([excess_kurtosis], index=['Excess Kurtosis'])
+    df_kurtosis_times_skew = pd.DataFrame([kurtosis_times_skew], index=['Kurtosis * Skew'])
+    df_excess_kurtosis_times_skew = pd.DataFrame([excess_kurtosis_times_skew], index=['Excess Kurtosis * Skew'])
+
+
+    extend_perf_stats = []
+    extend_perf_stats.extend(
+        [
+            df_average_return,
+            df_median_return,
+            df_max_return,
+            df_min_return,
+            df_st_dev,
+            df_average_return_over_st_dev,
+            df_gain_to_pain,
+            # df_information_ratio,
+            df_skew,
+            df_kurtosis,
+            df_excess_kurtosis,
+            df_kurtosis_times_skew,
+            df_excess_kurtosis_times_skew,
+        ]
+    )
+    performance_stats = pd.concat(extend_perf_stats)
+
+    print(f"performance_stats:\n{performance_stats}")
+    print(f"--------- END ---------")
+
 
 
 # plots(df_monthly)
