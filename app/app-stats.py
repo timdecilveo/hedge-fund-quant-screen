53d64462710c2955b0f09b3890745d4f8b675a91
Tim Decilveo <timothy.decilveo@gmail.com>
2021-12-30 08:33:44 -0800

    MAIN-added Stats().average_return_over_st_dev(), Stats().median_return_over_st_dev(), Stats().cagr_over_st_dev(), Stats().average_return_over_max_dd(), Stats().median_return_over_max_dd(), Stats().cagr_over_max_dd()

--- app/app-stats.py
+++ app/app-stats.py
@@ -1,18 +1,4 @@
-import os
-import numpy as np
-import pandas as pd
-from scipy.stats import norm, kurtosis
-import matplotlib.pyplot as plt
-import seaborn as sns
-from directories import Directory
 from stats import Stats
-
-beginning_date = '1993-02-28'
-ending_date = '2019-02-28'
-end_date = '2019-02-28'
-
-directory = os.listdir(f"../data-files")
-file_list = Directory(directory).files()
 
 average_return = Stats().average_return()
 median_return = Stats().median_return()
@@ -21,3 +7,13 @@ minimum_return = Stats().minimum_return()
 standard_devation = Stats().standard_devation()
 cagr = Stats().cagr()
 max_drawdown = Stats().max_drawdown()
+
+average_return_over_st_dev = Stats().average_return_over_st_dev()
+median_return_over_st_dev = Stats().median_return_over_st_dev()
+cagr_over_st_dev = Stats().cagr_over_st_dev()
+
+average_return_over_max_dd = Stats().average_return_over_max_dd()
+median_return_over_max_dd = Stats().median_return_over_max_dd()
+cagr_over_max_dd = Stats().cagr_over_max_dd()
+
+# print(cagr_over_st_dev)
--- app/stats.py
+++ app/stats.py
@@ -26,7 +26,7 @@ class Stats:
         for i in self.file_list:
             file_name = i.columns[0]
             median_reutrn_ = i[file_name].expanding().median()
-            i['MedianReturn'] = median_reutrn_
+            i['MedReturn'] = median_reutrn_
             db.append(i)
         return db
 
@@ -82,9 +82,80 @@ class Stats:
             peak = compounded_return.expanding(min_periods=1).max()
             drawdown = (compounded_return / peak) - 1
             max_drawdown = drawdown.min()
-            i['MaxDrawdown'] = max_drawdown
+            i['MaxDD'] = max_drawdown
             db.append(i)
         return db
     
-    # def st_dev_denominator(self):
+    def average_return_over_st_dev(self):
+        db = []
+        st_devs = self.standard_devation()
+        avg_returns = self.average_return()
+        for (avg_return, st_dev) in zip(avg_returns, st_devs):
+            avg_ret = avg_return['AvgReturn']
+            st_dev_ = st_dev['StDev']
+            df = pd.merge_ordered(avg_ret, st_dev_, on='Date')
+            df['AvgReturn / StDev'] = df['AvgReturn'] / df['StDev']
+            db.append(df)
+        return db
 
+    def median_return_over_st_dev(self):
+        db = []
+        st_devs = self.standard_devation()
+        median_returns = self.median_return()
+        for (median_return, st_dev) in zip(median_returns, st_devs):
+            median_ret = median_return['MedReturn']
+            st_dev_ = st_dev['StDev']
+            df = pd.merge_ordered(median_ret, st_dev_, on='Date')
+            df['MedReturn / StDev'] = df['MedReturn'] / df['StDev']
+            db.append(df)
+        return db
+
+    def cagr_over_st_dev(self):
+        db = []
+        st_devs = self.standard_devation()
+        cagrs = self.cagr()
+        for (cagr, st_dev) in zip(cagrs, st_devs):
+            cagr_ = cagr['CAGR']
+            st_dev_ = st_dev['StDev']
+            df = pd.merge_ordered(cagr_, st_dev_, on='Date')
+            df['CAGR / StDev'] = df['CAGR'] / df['StDev']
+            db.append(df)
+        return db
+
+    def average_return_over_max_dd(self):
+        db = []
+        max_dds = self.max_drawdown()
+        avg_returns = self.average_return()
+        for (avg_return, max_dd) in zip(avg_returns, max_dds):
+            avg_ret = avg_return['AvgReturn']
+            dd = max_dd['MaxDD']
+            df = pd.merge_ordered(avg_ret, dd, on='Date')
+            df['AvgReturn / MaxDD'] = df['AvgReturn'] / abs(df['MaxDD'])
+            db.append(df)
+        return db
+
+    def median_return_over_max_dd(self):
+        db = []
+        max_dds = self.max_drawdown()
+        median_returns = self.median_return()
+        for (median_return, max_dd) in zip(median_returns, max_dds):
+            median_ret = median_return['MedReturn']
+            dd = max_dd['MaxDD']
+            df = pd.merge_ordered(median_ret, dd, on='Date')
+            df['MedReturn / MaxDD'] = df['MedReturn'] / abs(df['MaxDD'])
+            db.append(df)
+        return db
+
+    def cagr_over_max_dd(self):
+        db = []
+        max_dds = self.max_drawdown()
+        cagrs = self.cagr()
+        for (cagr, max_dd) in zip(cagrs, max_dds):
+            cagr_ = cagr['CAGR']
+            dd = max_dd['MaxDD']
+            df = pd.merge_ordered(cagr_, dd, on='Date')
+            df['CAGR / MaxDD'] = df['CAGR'] / abs(df['MaxDD'])
+            db.append(df)
+        return db
+
+# gain to pain ratio - line 50
