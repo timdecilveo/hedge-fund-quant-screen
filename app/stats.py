5f9d1f64931e40dd7a68b64131f622468ff7e047
timdecilveo <timothy.decilveo@gmail.com>
2021-12-30 06:27:21 -0800

    MAIN-added drawdown method

--- app/stats.py
+++ app/stats.py
@@ -8,60 +8,83 @@ import datetime as dt
 from directories import Directory
 
 class Stats:
-    def __init__(self, beginning_date, end_date):
-        # self.df = pd.DataFrame()
+    def __init__(self):
         self.directory = os.listdir(f"../data-files")
         self.file_list = Directory(self.directory).files()
 
-        self.beginning_date = dt.datetime.strptime(beginning_date, '%Y-%m-%d')
-        self.end_date = dt.datetime.strptime(end_date, '%Y-%m-%d')
-
     def average_return(self):
-        rows = []
-        df = pd.DataFrame()
-
+        db = []
         for i in self.file_list:
-            column = i.columns[0]
-            avg_return = i[column].expanding().mean()
-            rows.append(avg_return)
-        
-        for row in rows:
-            print(f"row:\n{row}")
-            print("---------")
-            print(f"type(row):\n{type(row)}")
-            print("---------")
-        #     df['AverageReturn'] = row
-        # print(f"df:\n{df}")
-        # return df
+            file_name = i.columns[0]
+            average_return_ = i[file_name].expanding().mean()
+            i['AvgReturn'] = average_return_
+            db.append(i)
+        return db
 
-    # def median_return(self):
-    #     for i in self.file_list:
-    #         column = i.columns[0]
-    #         i['MedianReturn'] = i[column].expanding().median()
-    #         self.df = self.df.append(i['MedianReturn'], ignore_index=True)
-    #         # print(f"i:\n{i}")
-    #     return self.df
+    def median_return(self):
+        db = []
+        for i in self.file_list:
+            file_name = i.columns[0]
+            median_reutrn_ = i[file_name].expanding().median()
+            i['MedianReturn'] = median_reutrn_
+            db.append(i)
+        return db
 
-    # def max_return(self):
-    #     for i in self.file_list:
-    #         column = i.columns[0]
-    #         i['MaxReturn'] = i[column].expanding().max()
-    #         self.df = self.df.append(i['MaxReturn'], ignore_index=True)
-    #         # print(f"i:\n{i}")
-    #     return self.df
+    def nmaximum_return(self):
+        db = []
+        for i in self.file_list:
+            file_name = i.columns[0]
+            maximum_return_ = i[file_name].expanding().max()
+            i['MaxReturn'] = maximum_return_
+            db.append(i)
+        return db
 
-    # def min_return(self):
-    #     for i in self.file_list:
-    #         column = i.columns[0]
-    #         i['MinReturn'] = i[column].expanding().min()
-    #         self.df = self.df.append(i['MinReturn'], ignore_index=True)
-    #         # print(f"i:\n{i}")
-    #     return self.df
+    def minimum_return(self):
+        db = []
+        for i in self.file_list:
+            file_name = i.columns[0]
+            minimum_return_ = i[file_name].expanding().min()
+            i['MinReturn'] = minimum_return_
+            db.append(i)
+        return db
 
-    # def st_dev(self):
-    #     for i in self.file_list:
-    #         column = i.columns[0]
-    #         i['StDev'] = i[column].expanding().std()
-    #         self.df = self.df.append(i['StDev'], ignore_index=True)
-    #         # print(f"i:\n{i}")
-    #     return self.df
+    def standard_devation(self):
+        db = []
+        for i in self.file_list:
+            file_name = i.columns[0]
+            standard_deviation_ = i[file_name].expanding().std()
+            i['StDev'] = standard_deviation_
+            db.append(i)
+        return db
+    
+    def cagr(self):
+        db = []
+        for i in self.file_list:
+            file_name = i.columns[0]
+            dates = i[file_name].index
+            start_date = dates[0]
+            end_date = dates[-1] #need to fix this so it's the rolling date
+            time_delta_days = (end_date - start_date).days
+            years = time_delta_days / 365
+
+            compounded_return = (i[file_name] + 1).cumprod()
+            cagr = ((compounded_return) ** (1 / years)) - 1
+            i['CompoundedReturn'] = compounded_return
+            i['CAGR'] = cagr
+            db.append(i)
+        return db
+
+    def max_drawdown(self):
+        db = []
+        for i in self.file_list:
+            file_name = i.columns[0]
+            compounded_return = (i[file_name] + 1).cumprod()
+            peak = compounded_return.expanding(min_periods=1).max()
+            drawdown = (compounded_return / peak) - 1
+            max_drawdown = drawdown.min()
+            i['MaxDrawdown'] = max_drawdown
+            db.append(i)
+        return db
+    
+    # def st_dev_denominator(self):
+
