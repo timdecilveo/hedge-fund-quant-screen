5067117805c78d7489873c307affd893182333e2
Tim Decilveo <timothy.decilveo@gmail.com>
2022-01-01 09:16:31 -0800

    MAIN-updated file names and env

---
+++ app/app.py
@@ -0,0 +1,10 @@
+from stats import Stats
+
+
+statistics = Stats().statistics()
+# losing_months = Stats().losing_months()
+
+
+# downside_deviation = Stats().downside_deviation()
+
+print(statistics)
