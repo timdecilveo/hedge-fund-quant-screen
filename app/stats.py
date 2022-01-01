301093d959726d3dde97ae8b8e0b89767cd03810
Tim Decilveo <timothy.decilveo@gmail.com>
2022-01-01 09:13:01 -0800

    MAIN-adjusted code and added comments

--- app/stats.py
+++ app/stats.py
@@ -23,160 +23,102 @@ class Stats:
             start_date = dates[0]
             end_date = dates[-1] #need to fix this so it's the rolling date
 
-            monthly_return = i[file_name]
-            average_return = monthly_return.expanding().mean()
-            median_reutrn = monthly_return.expanding().median()
-            maximum_return = monthly_return.expanding().max()
-            minimum_return = monthly_return.expanding().min()
-            standard_deviation = monthly_return.expanding().std()
+            i['MonthlyReturn'] = i[file_name]
+            i['AvgReturn'] = i['MonthlyReturn'].expanding().mean()
+            i['MedianReturn'] = i['MonthlyReturn'].expanding().median()
+            i['MaxReturn'] =  i['MonthlyReturn'].expanding().max()
+            i['MinReturn'] = i['MonthlyReturn'].expanding().min()
+            i['StDev'] =  i['MonthlyReturn'].expanding().std()
+            i['Variance'] = i['MonthlyReturn'].expanding().var()
 
-            # downside deviation of monthly percentage returns
-            downside_deviation = i[monthly_return < 0].expanding().std() # deviations of only the negative returns
-            
-            # pd.merge_ordered(i, downside_deviation, on='Date', suffixes=('_win', '_downside_dev'), fill_method='ffill')
-            # print(type(downside_deviation))
+            # # downside deviation of monthly percentage returns
+            downside_deviation = i['MonthlyReturn'][i['MonthlyReturn'] < 0].expanding().std()
+            i['DownsideDev'] = downside_deviation
+            i['DownsideDev'].fillna(method="ffill", inplace=True)
 
-
-
-
-            skew = monthly_return.expanding().skew(axis=0)
-            kurtosis = monthly_return.expanding().kurt(axis=0)
-            excess_kurtosis = kurtosis - 3
-            kurtosis_times_skew_ = kurtosis * skew
-            excess_kurtosis_times_skew_ = excess_kurtosis * skew
+            i['Skew'] = i['MonthlyReturn'].expanding().skew(axis=0)
+            i['Kurtosis'] = i['MonthlyReturn'].expanding().kurt(axis=0)
+            i['Kurt*Skew'] = i['Kurtosis'] * i['Skew']
+            i['ExcessKurtosis'] = i['Kurtosis'] - 3
+            i['ExcessKurtosis*Skew'] = i['ExcessKurtosis'] * i['Skew']
 
             time_delta_days = (end_date - start_date).days
             years = time_delta_days / 365
-            compounded_return = (monthly_return + 1).cumprod()
-            cagr = ((compounded_return) ** (1 / years)) - 1
+            i['CompoundedReturn'] = (i['MonthlyReturn'] + 1).cumprod()
+            i['CAGR'] = ((i['CompoundedReturn']) ** (1 / years)) - 1
 
-            peak = compounded_return.expanding(min_periods=1).max()
-            drawdown = (compounded_return / peak) - 1
-            max_drawdown = drawdown.min()
-            markowitz_return_function = average_return / standard_deviation
+            peak = i['CompoundedReturn'].expanding(min_periods=1).max()
+            drawdown = (i['CompoundedReturn'] / peak) - 1
+            i['MaxDD'] = drawdown.min()
+            # # max_dd beginning date
+            # # max_dd ending date
+            # # max_dd in months
+            # # max_dd in years
+            # # max_dd duration
 
-            # ####################################
-            i['MonthlyReturn'] = monthly_return
-            i['AvgReturn'] = average_return
-            i['MedianReturn'] = median_reutrn
-            i['MaxReturn'] = maximum_return
-            i['MinReturn'] = minimum_return
-            i['StDev'] = standard_deviation
-            i['DownsideDev'] = downside_deviation
-            i['DownsideDev'].fillna(method="ffill", inplace=True)
-            i['Skew'] = skew
-            i['Kurtosis'] = kurtosis
-            i['ExcessKurtosis'] = excess_kurtosis
-            i['Kurt*Skew'] = kurtosis_times_skew_
-            i['ExcessKurtosis*Skew'] = excess_kurtosis_times_skew_
-            i['CompoundedReturn'] = compounded_return
-            i['CAGR'] = cagr
-            i['MaxDD'] = max_drawdown
-            i['MarkowitzReturnFunction'] = markowitz_return_function
+            i['MarkowitzReturnFunction'] = i['AvgReturn'] / i['Variance']
+            i['MarkowitzReturnFunction (CAGR)'] = i['CAGR'] / i['Variance']
+
             i['AvgReturn/StDev'] = i['AvgReturn'] / i['StDev']
             i['MedianReturn/StDev'] = i['MedianReturn'] / i['StDev']
             i['CAGR/StDev'] = i['CAGR'] / i['StDev']
+
             i['AvgReturn/MaxDD'] = i['AvgReturn'] / abs(i['MaxDD'])
             i['MedianReturn/MaxDD'] = i['MedianReturn'] / abs(i['MaxDD'])
             i['CAGR/MaxDD'] = i['CAGR'] / abs(i['MaxDD'])
-            # ####################################
 
-            '''
-            Sharpe Ratio:
-            sharpe ratio = (rp - rf) / standard deviation
-                rp = average_return
-                rf = self.rf
-                standard deviation = standard_deviation
-            '''
-            sharpe = (i['AvgReturn'] - self.rf) / i['StDev']
-            i['SharpeRatio'] = sharpe
-            i['SharpeRatioAnnualized'] = sharpe * np.sqrt(12)
+            # # ####################################
+            # # gain to pain ratio
+            # # information_ratio
+            # # tail_ratio
+            # # treynor_ratio
 
-            '''
-            Sortino Ratio:
-            sortino ratio = (rp - rf) / downside deviation
-                rp = average_return
-                rf = self.rf
-                downside deviation = downside_deviation
-            '''
-            sortino = (i['AvgReturn'] - self.rf) / i['DownsideDev']
-            i['SortinoRatio'] = sortino
-            i['SortinoRatioAnnualized'] = sortino * np.sqrt(12)
+            i['SharpeRatio'] = (i['AvgReturn'] - self.rf) / i['StDev']
+            i['SharpeRatio-Annualized'] = i['SharpeRatio'] * np.sqrt(12)
 
-            '''
-            Calmar Ratio:
-            calmar ratio = (rp - rf) / maximum drawdown
-                rp = average_return
-                rf = self.rf
-                maximum drawdown = max_drawdown
-            '''
-            calmar = (i['AvgReturn'] - self.rf) / abs(i['MaxDD'])
-            i['CalmarRatio'] = calmar
-            i['CalmarRatioAnnualized'] = calmar * np.sqrt(12)
+            i['SortinoRatio'] = (i['AvgReturn'] - self.rf) / i['DownsideDev']
+            i['SortinoRatio-Annualized'] = i['SortinoRatio'] * np.sqrt(12)
 
-            '''
-            Parametric/Gaussian VaR
-            gaussian var = 
-            '''
+            i['CalmarRatio'] = (i['AvgReturn'] - self.rf) / abs(i['MaxDD'])
+            i['CalmarRatio-Annualized'] = i['CalmarRatio'] * np.sqrt(12)
+
+            # # Parametric/Gaussian VaR
             z = norm.ppf(0.05)
-            gaussian_var = -(i['AvgReturn'] + z * i['MonthlyReturn'].std(ddof=0))
-            i['Gaussian VaR'] = gaussian_var
+            i['Gaussian VaR'] = -(i['AvgReturn'] + z * i['MonthlyReturn'].std(ddof=0))
 
-            '''
-            Modified Cornish Fisher VaR
-            cornish fisher var = 
-            '''
+            # Modified Cornish Fisher VaR
             z = (z + (z**2 - 1) * i['Skew'] / 6 + (z**3 - 3 * z) * (i['Kurtosis'] - 3) / 24 - (2 * z**3 - 5 * z) * (i['Skew']**2) / 36)
-            modified_cornish_fisher_var = -(i['AvgReturn'] + z * i['MonthlyReturn'].std(ddof=0))
-            i['CornishFisher VaR'] = modified_cornish_fisher_var
+            i['CornishFisher VaR'] = -(i['AvgReturn'] + z * i['MonthlyReturn'].std(ddof=0))
 
-            '''
-            Winning Months vs. Losing Months
-            '''
+            # Total Months
+            i['Total-NumOfPeriods'] = i['MonthlyReturn'].expanding().count()
+            
             # Winning Months
-            winning_return = i['MonthlyReturn'][i['MonthlyReturn'] > 0]
-            i['Winning-Return'] = winning_return
-
+            i['Winning-Return'] = i['MonthlyReturn'][i['MonthlyReturn'] > 0]
             i['Winning-AvgReturn'] = i['Winning-Return'].expanding().mean()
             i['Winning-MedianReturn'] = i['Winning-Return'].expanding().median()
             i['Winning-MaxReturn'] = i['Winning-Return'].expanding().max()
             i['Winning-MinReturn'] = i['Winning-Return'].expanding().min()
             i['Winning-StDev'] = i['Winning-Return'].expanding().std()
+            i['Winning-Variance'] = i['Winning-Return'].expanding().var()
             i['Winning-NumOfPeriods'] = i['Winning-Return'].expanding().count()
+            i['WinningPerc (%)'] = i['Winning-NumOfPeriods'] / i['Total-NumOfPeriods']
 
             # Losing Months
-            losing_return = i['MonthlyReturn'][i['MonthlyReturn'] < 0]
-            i['Losing-Return'] = losing_return
-
+            i['Losing-Return'] = i['MonthlyReturn'][i['MonthlyReturn'] < 0]
             i['Losing-AvgReturn'] = i['Losing-Return'].expanding().mean()
             i['Losing-MedianReturn'] = i['Losing-Return'].expanding().median()
             i['Losing-MaxReturn'] = i['Losing-Return'].expanding().max()
             i['Losing-MinReturn'] = i['Losing-Return'].expanding().min()
             i['Losing-StDev'] = i['Losing-Return'].expanding().std()
+            i['Losing-Variance'] = i['Losing-Return'].expanding().var()
             i['Losing-NumOfPeriods'] = i['Losing-Return'].expanding().count()
-
-            i['Total-NumOfPeriods'] = i['MonthlyReturn'].expanding().count()
-
-            i['WinningPerc (%)'] = i['Winning-NumOfPeriods'] / i['Total-NumOfPeriods']
             i['LosingPerc (%)'] = i['Losing-NumOfPeriods'] / i['Total-NumOfPeriods']
+
+            # Win-Loss Calcs
             i['Win-Loss Ratio'] = i['Winning-NumOfPeriods'] / i['Losing-NumOfPeriods']
             i['Expectancy (+)'] = (i['Winning-MedianReturn'] * i['Winning-NumOfPeriods']) + (i['Losing-MedianReturn'] * i['Losing-NumOfPeriods'])
             i['ExepectancyRatio (%)'] = (i['Winning-MedianReturn'] * i['Winning-NumOfPeriods']) / abs((i['Losing-MedianReturn'] * i['Losing-NumOfPeriods']))
 
-            # ####################################
-            # gain to pain ratio
-            # information_ratio
-
-            
-            db.append(i)
-        return db
-
-    def downside_deviation(self):
-        db = []
-
-        for i in self.file_list:
-            file_name = i.columns[0]
-            downside_deviation_ = i[i[file_name] < 0].expanding().std() # deviations of only the negative returns
-            i['DownsideDev'] = downside_deviation_
             db.append(i)
         return db
