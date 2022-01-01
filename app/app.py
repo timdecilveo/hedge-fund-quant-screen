8648008d871c5ae7b152aabb8753043655047c9c
Tim Decilveo <timothy.decilveo@gmail.com>
2022-01-01 09:58:51 -0800

    MAIN-updated file

--- app/app.py
+++ app/app.py
@@ -1,10 +1,4 @@
 from monthlyStatistics import MonthlyStatistics
 
-
 statistics = MonthlyStatistics().statistics()
-# losing_months = Stats().losing_months()
-
-
-# downside_deviation = Stats().downside_deviation()
-
 print(statistics)
